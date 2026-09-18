c1d4b6e84c0a4fc6d8d62f45c54829a09a84cb8a
firsmine <sitishafira.fia@gmail.com>
2026-09-18 09:57:32 +0800

    1

---
+++ Console.js
@@ -0,0 +1,18 @@
+/* Browser pada umumnya mempunyai tab console yang bisa digunakan untuk menampilkan data console.log dari kode JavaScript. Untuk mengaktifkan tab console di Chrome, bisa dilakukan dengan pilih menu View -> Developer -> Developer Tools atau dengan tekan tombol F12 atau kombinasi tombol Ctrl-Shift-I 
+
+console.log adalah salah satu syntax JavaScript untuk menampilkan nilai suatu variabel atau hasil operasi di tab console.
+
+Dengan console.log, output dari variabel atau operasi yang kita masukkan tidak akan kelihatan di halaman website dan hanya bisa tampil di tab console. Dengan begitu, output tersebut tidak akan mengganggu tampilan website kamu.
+
+console.log sering digunakan developer ketika mau debug/investigasi kesalahan di kode yang ditulis.
+*/
+
+let nama = "Budi";
+let umur = 20;
+let apakahSudahMenikah = false;
+let jumlahAnak;
+
+console.log(nama); // output: Budi
+console.log(umur); // output: 20
+console.log(apakahSudahMenikah); // // output: false
+console.log(jumlahAnak); // output: undefined
